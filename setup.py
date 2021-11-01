0d0f52fbac603049283b9750c4997246752b1511
John Esther <55128894+estaar@users.noreply.github.com>
2021-11-01 16:19:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 
+from setuptools import setup
+
+setup(
+    name='bipscan',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:estaar/bipscan.git',
+    author='John Esther',
+    author_email='estaarjohn@gmail.com',
+    license='unlicense',
+    packages=['bipscan'],
+    zip_safe=False
+)
